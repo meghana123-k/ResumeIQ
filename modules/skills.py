89607be5bdca4491e8b7faa7d4a0f328a0be31d7
meghana123-k <kothakondameghana3@gmail.com>
2026-01-16 15:00:33 +0530

    Modified skills.py to handle canonical forms

--- modules/skills.py
+++ modules/skills.py
@@ -3,7 +3,6 @@ SKILLS = [
     "html", "css", "flask", "spring boot",
     "mysql", "mongodb", "sql",
     "data structures", "algorithms",
-    "oop", "object oriented programming",
     "rest api", "git", "github",
     "postman",
     "manual testing", "regression testing", "sdlc", "stlc"
